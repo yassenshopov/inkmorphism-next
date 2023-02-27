2873fd5ad4adb47aaad81f61fee9576df619a929
Yassen Shopov <yassen.shopov@nuvei.com>
2023-02-27 18:28:27 +0200

    merge attempt

--- firebase/clientApp.js
+++ firebase/clientApp.js
@@ -1,6 +1,15 @@
 import { getApp, initializeApp } from "firebase/app";
 
 const clientCredentials = {
+    // // For DEV:
+    // apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    // authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    // projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    // storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    // messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    // appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID, 
+
+    // // For PROD:
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
     projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
