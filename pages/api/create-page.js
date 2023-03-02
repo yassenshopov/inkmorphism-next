7fb1cc2083d80e41d9f03663e3e7f0fe6577fbfd
Yassen Shopov <yassen.shopov@nuvei.com>
2023-03-02 14:35:21 +0200

    Initialised cPanel

--- pages/api/create-page.js
+++ pages/api/create-page.js
@@ -3,23 +3,25 @@
 import fs from 'fs';
 
 export default function handler(req, res) {
-  const { folderName, pageTitle } = req.body;
+    const { folderName, pageTitle } = req.body;
 
-  // create new folder
-  fs.mkdirSync(`pages/${folderName}`);
+    let pageTitleUp = pageTitle.charAt(0).toUpperCase() + pageTitle.slice(1); 
+    //   create new folder
+    fs.mkdirSync(`pages/clients/${folderName}`);
+    console.log("1st half YES")
 
-  // create new page file
-  fs.writeFileSync(
-    `pages/${folderName}/index.js`,
-    `export default function ${pageTitle}() {
-      return (
+    // create new page file
+    fs.writeFileSync(
+    `pages/clients/${folderName}/index.js`,
+    `export default function Default() {
+        return (
         <div>
-          <h1>${pageTitle}</h1>
-          <p>This is a dynamically generated page.</p>
+            <h1>${pageTitle}</h1>
+            <p>This is a dynamically generated page.</p>
         </div>
-      );
+        );
     }`
-  );
+    );
 
   res.status(200).json({ message: 'Page created successfully.' });
 }
