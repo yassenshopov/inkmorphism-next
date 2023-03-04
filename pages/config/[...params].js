afcb62c77dce35866a80e36f9f9ea14162c53170
Yassen Shopov <yassen.shopov@nuvei.com>
2023-03-04 12:18:35 +0200

    Fixed rules for database READ

--- pages/config/[...params].js
+++ pages/config/[...params].js
@@ -1,8 +1,10 @@
 import Editor from "../Editor";
 import { useRouter } from "next/router";
+import app from '../../firebase/clientApp';
+import { getAuth } from 'firebase/auth'
 
 export default function configSite() {
-
+    const auth = getAuth(app);
     const router = useRouter();
     const {params = []} = router.query;
 
@@ -11,6 +13,7 @@ export default function configSite() {
             {/* <h1>{params}</h1> */}
             <Editor 
                 name={params[0]}
+                auth={auth}
             />
         </>
     );
---
+++ pages/sites/data-frosty-234/index.js
@@ -0,0 +1,8 @@
+export default function Default() {
+        return (
+        <div>
+            <h1>The Data Chunk</h1>
+            <p>This is a dynamically generated page.</p>
+        </div>
+        );
+    }
