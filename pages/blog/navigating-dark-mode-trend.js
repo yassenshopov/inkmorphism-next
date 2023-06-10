d583e5d1e3edbe858b29bfb6cc9cb6c356d33ccc
Yassen Shopov <yassen.shopov@nuvei.com>
2023-06-10 14:29:53 +0300

    Initialised new article

---
+++ pages/blog/navigating-dark-mode-trend.js
@@ -0,0 +1,89 @@
+
+    import Head from "next/head";
+    import { useEffect, useState } from "react";
+    import MainNav from "../components/MainNav";
+    import MainFooter from "../components/MainFooter";  
+    import ReactMarkdown from "react-markdown";
+  
+    export default function Article() {
+      const [authorData, setAuthorData] = useState({
+        authorProfilePic: "",
+        authorBio: "",
+        authorSocials: [],
+        author: "",
+      });
+      const [processedMD, setProcessedMD] = useState("");
+
+      useEffect(() => {
+        const getStatic = () => {
+          importData();
+        };
+        getStatic();
+      }, []);
+      const importData = async () => {
+          try {
+            const jsonData = await import('./authors/yassenshopov.json');
+            setAuthorData(jsonData);
+            const md = await import('./raw_files/navigating-dark-mode-trend.md');
+            setProcessedMD(<ReactMarkdown>{md.default.replace(
+              "%placeholder%",
+              "https://firebasestorage.googleapis.com/v0/b/inkmorphism.appspot.com/o/blog%2Fnavigating-dark-mode-trend%2Fthumbnail.webp?alt=media&token=ba740e3e-4ff7-4321-a7f9-f7e876409a8a&_gl=1*1tospw6*_ga*MTE1NDIyNjIyMi4xNjczMTk2MTM5*_ga_CW55HF8NVT*MTY4NjM5NDIzOS45MS4xLjE2ODYzOTYyOTMuMC4wLjA."
+            )}</ReactMarkdown>)
+          } catch (err) {
+            console.log(err);
+          }
+      };
+
+          return (
+            <div className={"Article"}>
+              <Head>
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <title>Navigating the Dark Mode Trend: Designing Websites for Eye Comfort</title>
+                <meta name="author" content="Yassen Shopov"></meta>
+                <meta name="description" content="Discover the benefits of dark mode in web design and learn how to design websites that prioritize eye comfort. Navigate the dark mode trend with practical tips and techniques." />
+                <link rel="icon" href="/faviconWh.ico" />
+                <meta property="og:image" content="https://firebasestorage.googleapis.com/v0/b/inkmorphism.appspot.com/o/blog%2Fnavigating-dark-mode-trend%2Fthumbnail.webp?alt=media&token=ba740e3e-4ff7-4321-a7f9-f7e876409a8a&_gl=1*1tospw6*_ga*MTE1NDIyNjIyMi4xNjczMTk2MTM5*_ga_CW55HF8NVT*MTY4NjM5NDIzOS45MS4xLjE2ODYzOTYyOTMuMC4wLjA."></meta>
+                <meta property="og:type" content="website"></meta>
+                <meta property="og:title" content="Navigating the Dark Mode Trend: Designing Websites for Eye Comfort"></meta>
+                <meta property="og:description" content="Discover the benefits of dark mode in web design and learn how to design websites that prioritize eye comfort. Navigate the dark mode trend with practical tips and techniques."></meta>
+                <meta property="twitter:card" content="summary_large_image"></meta>
+                <meta property="twitter:title" content="Navigating the Dark Mode Trend: Designing Websites for Eye Comfort"></meta>
+                <meta property="twitter:description" content="Discover the benefits of dark mode in web design and learn how to design websites that prioritize eye comfort. Navigate the dark mode trend with practical tips and techniques."></meta>
+              </Head>
+            
+              <MainNav />
+          
+              <main>
+                <div id="thumbnail">
+                  <img src="https://firebasestorage.googleapis.com/v0/b/inkmorphism.appspot.com/o/blog%2Fnavigating-dark-mode-trend%2Fthumbnail.webp?alt=media&token=ba740e3e-4ff7-4321-a7f9-f7e876409a8a&_gl=1*1tospw6*_ga*MTE1NDIyNjIyMi4xNjczMTk2MTM5*_ga_CW55HF8NVT*MTY4NjM5NDIzOS45MS4xLjE2ODYzOTYyOTMuMC4wLjA." />
+                </div>
+                <article id="article">{
+                  processedMD
+                }
+                </article>
+                <section id="authorProfile">
+                  <div id="authorProfilePic">
+                    <img loading="lazy" src={authorData.authorProfilePic} />
+                  </div>
+                  <div id="authorProfileInfo">
+                    <h3>Yassen Shopov</h3>
+                    <p>{authorData.authorBio}</p>
+                    <div id="socials">
+                    {
+                      authorData.authorSocials.map((social, index) => {
+                        return (
+                          <a href={social.url} target="_blank" key={index} className="noSelect">
+                            <img loading="lazy" src={social.icon} />
+                          </a>
+                        )
+                      })
+                    }
+                    </div>
+                  </div>
+                </section>
+              </main>
+          
+              <MainFooter />
+            </div>
+          );
+      }
---
+++ pages/blog/raw_files/navigating-dark-mode-trend.md
@@ -0,0 +1,42 @@
+# Navigating the Dark Mode Trend: Designing Websites for Eye Comfort
+
+![Image](%placeholder%)
+
+A. Explanation of the rising popularity of dark mode in web design
+B. Importance of prioritizing eye comfort in website design
+
+## Understanding Dark Mode
+A. Definition and characteristics of dark mode
+B. Benefits of dark mode for users
+C. Impact of dark mode on reducing eye strain and fatigue
+
+## Design Considerations for Dark Mode Websites
+A. Color choices and contrast ratios for optimal readability
+B. Typography selection and adjustments for enhanced legibility
+C. Icon and graphic design techniques for visibility in dark mode
+D. Maintaining visual hierarchy and organization in dark interfaces
+
+## Implementing Dark Mode Responsively
+A. CSS media queries and techniques for detecting user preferences
+B. Designing adaptable user interfaces that seamlessly transition between light and dark modes
+C. Ensuring accessibility in dark mode for users with visual impairments
+
+## User Experience in Dark Mode
+A. User research and testing to gather feedback on dark mode experiences
+B. Customization options and user preferences for dark mode settings
+C. Consistency and familiarity in dark mode across different devices and platforms
+
+## Best Practices for Dark Mode Implementation
+A. Recommendations for integrating dark mode into existing websites
+B. Tips for maintaining brand identity and aesthetics in dark mode designs
+C. Considerations for dark mode in specific website components (e.g., forms, images, navigation menus)
+
+## Future Trends and Technologies in Dark Mode
+A. Advancements in automatic dark mode detection and switching
+B. Potential integration of dark mode with other emerging technologies (e.g., augmented reality)
+C. Evolving user preferences and expectations in dark mode experiences
+
+## Conclusion
+A. Recap of the benefits of dark mode for eye comfort and user experience
+B. Encouragement for web designers to embrace the dark mode trend
+C. Final thoughts on the future of dark mode in web design
