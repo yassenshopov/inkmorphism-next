9d5eec94d3f6b1bb5563516cce46758c51f0726b
Yassen Shopov <yassen.shopov@nuvei.com>
2023-08-04 23:23:35 +0300

    Hard-code key

--- stripe/initializeStripe.js
+++ stripe/initializeStripe.js
@@ -4,7 +4,8 @@ let stripePromise;
 
 const initializeStripe = async () => {
     if (!stripePromise) {
-        stripePromise = await loadStripe(`${process.env.STRIPE_PUBLISHABLE_KEY}`);
+        // stripePromise = await loadStripe(`${process.env.STRIPE_PUBLISHABLE_KEY}`);
+        stripePromise = await loadStripe("pk_live_51NHrXlCwphplMhaRwvZCW8gHqxpHfgXaqBjfALA0yj1uF2Gp8gtfJPoJx9baGYTkc0s7aDyCGsn12r5M8eatpGBJ00UPbo3kNl");
     }
     return stripePromise;
 };
