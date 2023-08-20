867d3e0141e036f63d65cde4ff9b8447bc5d22bf
Yassen Shopov <yassen.shopov@nuvei.com>
2023-08-20 14:27:17 +0300

    Added GA4 + fixed error on register

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,22 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+      <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-45TFYSTFLF"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){window.dataLayer.push(arguments)}
+            gtag('js', new Date());
+            gtag('config', 'G-45TFYSTFLF');
+        `,
+          }}
+        ></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
