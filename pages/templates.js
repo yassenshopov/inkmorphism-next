c9bd01a50a2bc146ea947b4f3c611eba846cfab3
Yassen Shopov <yassen.shopov@nuvei.com>
2023-09-06 19:27:32 +0300

    Created a one-time survey at "Templates"

--- pages/templates.js
+++ pages/templates.js
@@ -1,21 +1,9 @@
 import Head from "next/head";
-import Link from "next/link";
-import Dashnav from "./components/dashnav.js";
-import Dashfooter from "./components/dashfooter.js";
-import { randomWords } from "random-words";
-import {
-  getFirestore,
-  collection,
-  getDocs,
-  doc,
-  setDoc,
-} from "firebase/firestore/lite";
+import { getFirestore, collection, doc, setDoc, getDoc } from "firebase/firestore/lite";
 import { getAuth } from "firebase/auth";
 import app from "../firebase/clientApp";
 import { useState, useEffect } from "react";
-import placeholder from "../styles/images/placeholder.png";
 import defaultProfilePic from "../styles/images/defaultProfilePic.png";
-import defaultLogo from "../styles/images/logoPlace.png";
 import { getDownloadURL, getStorage, ref, uploadBytes } from "firebase/storage";
 import Loader from "./components/loader.js";
 import MainNav from "./components/MainNav.js";
@@ -27,10 +15,19 @@ let col;
 let slug;
 
 export default function Templates() {
+  // Meta data:
+  let title = "Inkmorphism - Templates for every website need";
+  let img = "https://inkmorphism.com/pricing/og.webp";
+  let description =
+    "Explore Inkmorphism's AI-powered website templates – Effortlessly create stunning sites with our innovative design solutions. Get started today!";
+  let author = "Yassen Shopov";
+
   let profile_pic = "";
 
   const [loadBool, setLoadBool] = useState(false);
   const [isUser, setIsUser] = useState(false);
+
+  const [recommendedTemplates, setRecommendedTemplates] = useState([]);
 
   useEffect(() => {
     const el = document.getElementById("fetch");
@@ -45,11 +42,9 @@ export default function Templates() {
     profile_pic: defaultProfilePic.src,
     displayName: "Default",
   });
-
-  console.log(userData);
+  let db = getFirestore(app);
 
   async function getData() {
-    let db = getFirestore(app);
     const auth = getAuth(app);
     let uid;
     try {
@@ -57,6 +52,22 @@ export default function Templates() {
       setIsUser(true);
     } catch (err) {
       uid = "_";
+    }
+    //check if user has filled out the survey
+    try {
+      const userSurveyData = doc(db, "users", uid, "surveyData", "data");
+      try {
+        const userSurveyResults = await getDoc(
+          doc(db, "users", uid, "surveyData", "results")
+        );
+        setRecommendedTemplates(userSurveyResults.data().recommendedTemplates);
+      } catch(err) {
+        setRecommendedTemplates([]);
+      }
+      setSurveyNotFilled(false);
+    } catch (err) {
+      console.log(err);
+      setSurveyNotFilled(true);
     }
     const storage = getStorage();
     const storageRef = ref(storage, uid + "/profilePic.png");
@@ -86,6 +97,11 @@ export default function Templates() {
         } catch (err) {}
       });
   }
+
+  useEffect(() => {
+    //check if a template from templatesArr is in the recommendedTemplates array
+    //if it is,
+  }, [recommendedTemplates]);
 
   try {
     profile_pic = auth.currentUser.photoURL;
@@ -224,145 +240,239 @@ export default function Templates() {
     }
   }, [defaultThumbnail]);
 
-  const templatesArr = ["industrial", "simple", "glassmorphism", "minimalism", "neobrutalism", "web3"];
+  const templatesArr = [
+    "industrial",
+    "simple",
+    "glassmorphism",
+    "minimalism",
+    "neobrutalism",
+    "web3",
+  ];
   const templates = templatesArr.map((template, index) => (
-    <article key={index}>
+    <article
+      key={index}
+      className={recommendedTemplates.includes(template) ? "recommended" : ""}
+    >
       <div className="imgWrapper">
         <img src={"main/" + template + "Thumbnail.png"} />
         <div id="hiddenBtns">
-        <a
-          className="noSelect"
-          onClick={() => {
-            if (isUser) {
-              randomSiteGen(template);
-            } else {
-              window.location.href = "/login";
-            }
-          }}
-        >
-          Get Started with <em>{template}</em> →
-        </a>
-        <a href={"templates/" + template} className="noSelect">
-          Preview Template
-        </a>
+          <a
+            className="noSelect"
+            onClick={() => {
+              if (isUser) {
+                randomSiteGen(template);
+              } else {
+                window.location.href = "/login";
+              }
+            }}
+          >
+            Get Started with <em>{template}</em> →
+          </a>
+          <a href={"templates/" + template} className="noSelect">
+            Preview Template
+          </a>
+        </div>
       </div>
-      </div>
-      <h2>{template.slice(0, 1).toUpperCase() + template.slice(1)}</h2>
+      <h2>{template.slice(0, 1).toUpperCase() + template.slice(1) + `${recommendedTemplates.includes(template) ? " (Recommended for you)" : ""}`}</h2>
     </article>
   ));
+
+  const [showInput, setShowInput] = useState(false);
+  const [selectedOption, setSelectedOption] = useState("");
+
+  const handleSelectChange = (event) => {
+    const selectedOption = event.target.value;
+    // Check if the selected option should show the input field
+    setShowInput(selectedOption === "other");
+    setSelectedOption(selectedOption);
+    if (selectedOption !== "default") {
+      setCanProceed(true);
+    } else {
+      setCanProceed(false);
+    }
+  };
+
+  const [canProceed, setCanProceed] = useState(false);
+  const [surveyQuestionIndex, setSurveyQuestionIndex] = useState(0);
+  const [surveyNotFilled, setSurveyNotFilled] = useState(false);
+  const [surveyData, setSurveyData] = useState({});
+
+  const questionArr = [
+    {
+      question: "What is the name for your website?",
+      type: "input",
+      propName: "name",
+      placeholder: "Enter the name of your website...",
+    },
+    {
+      question: "What is the primary goal of your website?",
+      type: "select",
+      propName: "goal",
+      options: [
+        { txt: "Choose option...", value: "default" },
+        { txt: "To sell services", value: "To sell services" },
+        { txt: "To sell products", value: "To sell products" },
+        { txt: "To showcase a portfolio", value: "To showcase a portfolio" },
+        { txt: "To showcase a blog", value: "To showcase a blog" },
+        { txt: "To showcase a gallery", value: "To showcase a gallery" },
+        { txt: "Other", value: "other" },
+      ],
+    },
+    {
+      question: "Who is your target audience?",
+      type: "select",
+      propName: "audience",
+      options: [
+        { txt: "Choose option...", value: "default" },
+        { txt: "General public", value: "General public" },
+        { txt: "Business professionals", value: "Business professionals" },
+        { txt: "Students", value: "Students" },
+        { txt: "Hobbyists", value: "Hobbyists" },
+        { txt: "Other", value: "other" },
+      ],
+    },
+    {
+      question:
+        "What keywords would you use to describe your website? (separate with commas)",
+      type: "input",
+      propName: "keywords",
+      placeholder: "Enter keywords...",
+    },
+    {
+      question: "Do you have any specific design preferences for your website?",
+      type: "select",
+      propName: "designPreferences",
+      options: [
+        { txt: "Choose option...", value: "default" },
+        { txt: "Minimalistic and clean", value: "Minimalistic and clean" },
+        { txt: "Colorful and vibrant", value: "Colorful and vibrant" },
+        { txt: "Elegant and professional", value: "Elegant and professional" },
+        { txt: "Other", value: "other" },
+      ],
+    },
+    {
+      question: "When a user visits your website, what do you want them to do?",
+      type: "select",
+      propName: "cta",
+      options: [
+        { txt: "Choose option...", value: "default" },
+        { txt: "Buy a product", value: "Buy a product" },
+        { txt: "Book a service", value: "Book a service" },
+        { txt: "Read a blog post", value: "Read a blog post" },
+        { txt: "Explore my website", value: "Explore my website" },
+        { txt: "Join my newsletter", value: "Join my newsletter" },
+        { txt: "Other", value: "other" },
+      ],
+    },
+  ];
 
   return (
     <div className="Templates">
       <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>{title}</title>
+        <meta name="author" content={author}></meta>
+        <meta name="description" content={description} />
         <link rel="icon" href="/faviconWh.ico" />
-        <title>Inkmorphism - Templates for your websites!</title>
+        <meta property="og:image" content={img}></meta>
+        <meta property="og:type" content="website"></meta>
+        <meta property="og:title" content={title}></meta>
+        <meta property="og:description" content={description}></meta>
+        <meta property="twitter:card" content="summary_large_image"></meta>
+        <meta property="twitter:title" content={title}></meta>
+        <meta property="twitter:description" content={description}></meta>
       </Head>
 
       {loadBool ? <Loader /> : ""}
 
+      {isUser && surveyNotFilled ? (
+        <div id="popUpSurvey" className="">
+          <p>
+            Question {surveyQuestionIndex + 1}/{questionArr.length}
+          </p>
+          <h2>{questionArr[surveyQuestionIndex].question}</h2>
+
+          <form id="popUpSurveyOptions">
+            {questionArr[surveyQuestionIndex].type === "select" ? (
+              <>
+                <select onChange={handleSelectChange} value={selectedOption}>
+                  {questionArr[surveyQuestionIndex].options.map(
+                    (option, index) => (
+                      <option key={index} value={option.value}>
+                        {option.txt}
+                      </option>
+                    )
+                  )}
+                </select>
+                {showInput && <input type="text" placeholder="Enter..." />}
+              </>
+            ) : questionArr[surveyQuestionIndex].type === "input" ? (
+              <input
+                type="text"
+                placeholder={questionArr[surveyQuestionIndex].placeholder}
+                onChange={(e) => {
+                  if (e.target.value !== "") {
+                    setCanProceed(true);
+                  } else {
+                    setCanProceed(false);
+                  }
+                }}
+              />
+            ) : (
+              ""
+            )}
+
+            <button
+              disabled={!canProceed}
+              onClick={(e) => {
+                // prevent Default
+                e.preventDefault();
+                if (surveyQuestionIndex < questionArr.length - 1) {
+                  setSurveyQuestionIndex(surveyQuestionIndex + 1);
+                  setCanProceed((prevState) => !prevState);
+                  setShowInput(false);
+                  setSelectedOption("");
+                  setSurveyData({
+                    ...surveyData,
+                    [questionArr[surveyQuestionIndex].propName]:
+                      selectedOption || document.querySelector("input").value,
+                  });
+                } else {
+                  setSurveyNotFilled(false);
+                  // send the filled out surveydata to firestore under user-uid/surveyData
+                  let db = getFirestore(app);
+                  const auth = getAuth(app);
+                  let uid;
+                  try {
+                    uid = "user-" + auth.currentUser.uid;
+                  } catch (err) {
+                    uid = "_";
+                  }
+                  const col = collection(db, `users/` + uid + `/surveyData`);
+                  setTimeout(() => {
+                    setDoc(doc(col, "data"), surveyData);
+                  }, 1000);
+                }
+              }}
+            >
+              Next <span>→</span>
+            </button>
+          </form>
+        </div>
+      ) : (
+        ""
+      )}
+
       <button id="fetch" onClick={getData}></button>
 
-      {/* <Dashnav profile_pic={userData.profile_pic} auth={auth} /> */}
-      < MainNav />
+      <MainNav />
       <main id="templatesWrapper">
         <section>
           <h1>Templates</h1>
         </section>
-        <section id="templates">
-          {templates}
-          {/* <article>
-            <img src="templates/simpleTemplate.png" />
-            <h2>Simple</h2>
-            <div id="hiddenBtns">
-              <a
-                onClick={() => {
-                  randomSiteGen("Skeletal");
-                }}
-              >
-                Get Started with Simple →
-              </a>
-              <a href="templates/simple">Preview Template</a>
-            </div>
-          </article> */}
-          {/* <article>
-            <img src="templates/neobrutalismTemplate2.png" />
-            <h2>Neobrutalism</h2>
-            <div id="hiddenBtns">
-              <a
-                onClick={() => {
-                  randomSiteGen("Neobrutalism");
-                }}
-              >
-                Get Started with Neobrutalism →
-              </a>
-              <a href="templates/neobrutalism">Preview Template</a>
-            </div>
-          </article>
-          <article>
-            <img src="templates/glassmorphismTemplate.png" />
-            <h2>Glassmorphism</h2>
-            <div id="hiddenBtns">
-              <a
-                onClick={() => {
-                  randomSiteGen("Glassmorphism");
-                }}
-              >
-                Get Started with glassmorphism →
-              </a>
-              <a href="templates/glassmorphism">Preview Template</a>
-            </div>
-          </article>
-          <article>
-            <img src="templates/web3Template.png" />
-            <h2>Web3</h2>
-            <div id="hiddenBtns">
-              <a
-                onClick={() => {
-                  randomSiteGen("Web3");
-                }}
-              >
-                Get Started with Web3 →
-              </a>
-              <a href="templates/web3">Preview Template</a>
-            </div>
-          </article>
-          <article>
-            <img src="templates/futurismTemplate.png" />
-            <h2>Futurism</h2>
-            <div id="hiddenBtns">
-              <a
-                onClick={() => {
-                  randomSiteGen("Futurism");
-                }}
-              >
-                Get Started with Futurism →
-              </a>
-              <a href="templates/futurism">Preview Template</a>
-            </div>
-          </article>
-          <article>
-            <img src="templates/minimalismTemplate.png" />
-            <h2>Minimalism</h2>
-            <div id="hiddenBtns">
-              <a
-                onClick={() => {
-                  randomSiteGen("Minimalism");
-                }}
-              >
-                Get Started with Minimalism →
-              </a>
-              <a href="templates/minimalism">Preview Template</a>
-            </div>
-          </article>
-          <article>
-            <h2>Isometrism</h2>
-          </article>
-          <article>
-            <h2>Fantasm</h2>
-          </article> */}
-        </section>
+        <section id="templates">{templates}</section>
       </main>
-      < MainFooter />
+      <MainFooter />
     </div>
   );
 }
